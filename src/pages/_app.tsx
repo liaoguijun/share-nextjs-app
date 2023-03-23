9b8a98b1b668d7313a2c6e5cd8fea8d428976bb0
ben <liaoguijunchn@gmail.com>
2023-03-23 16:05:37 +0800

    feat: 初始化

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Menu from "@/component/Menu";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps, router }: AppProps) {
+  if(router.route === "/login") {
+    return <Component {...pageProps} />
+  }
+  return (
+    <>
+      <Menu />
+      <Component {...pageProps} />
+    </>
+  );
 }
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,10 @@
+
+import styles from './styles/index.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      login
+    </div>
+  )
+}
